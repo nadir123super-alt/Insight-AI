ff98dc3bc62ffd3176650a1b4801a953aa650eb4
nadir123super-alt <nadir123super@gmail.com>
2026-04-12 17:34:25 +0800

    design: 进化为学术生产力风格 UI

---
+++ .wrangler/tmp/pages-3EHe7c/functionsRoutes-0.23434974156475574.mjs
@@ -0,0 +1,11 @@
+import { onRequest as __api_analyze_js_onRequest } from "C:\\Users\\Lenovo\\Desktop\\Insight-AI\\functions\\api\\analyze.js"
+
+export const routes = [
+    {
+      routePath: "/api/analyze",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_analyze_js_onRequest],
+    },
+  ]
